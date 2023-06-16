a5ddb19729a23900887d299ffc73d87e35459ca0
Nils Jacobsen <58360188+NilsJacobsen@users.noreply.github.com>
2023-06-16 09:06:35 +0200

    Update inlang.config.js

--- inlang.config.js
+++ inlang.config.js
@@ -12,7 +12,7 @@ export async function defineConfig(env) {
 		referenceLanguage: "en",
 		plugins: [
 			i18nextPlugin({
-				pathPattern: "./packages/apps/public/locales/{language}/translation.json",
+				pathPattern: "./packages/apps/public/locales/{language}/*.json",
 			}), standardLintRules(),
 		],
 	}
